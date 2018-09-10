363279e3f8d2b596971093a4773f9172cc4df79c
Gjum <code.gjum@gmail.com>
2018-09-10 22:01:36 +0200

    use reactions, cmd prefix is comma

--- civcord/cmd_handler.py
+++ civcord/cmd_handler.py
@@ -5,6 +5,7 @@ import sys
 from collections import namedtuple
 from importlib import reload
 
+import emojis
 import world
 from utils import Autodict
 
@@ -18,32 +19,39 @@ ttk2_strings = [l.strip().replace(r'\n', '\n') for l in open(
     'ttk2_reddit_civcraft_1k.phrases').read().split('\n') if l.strip()]
 
 
-Command = namedtuple('Command', 'name func description hidden')
+Command = namedtuple('Command', 'func name args desc hidden')
 
 commands = {}
 
 
-def register_cmd(name, description=None, hidden=False):
+def register_cmd(name, args=None, desc=None, hidden=False):
     def decorate(func):
-        commands[name] = Command(name, func, description, hidden)
+        commands[name] = Command(func, name, args, desc, hidden)
         return func
     return decorate
 
 
 def fmt_cmd_usage(state, name):
-    description = commands[name].description or name
-    return '{state.prefix}{description}'.format(**locals())
+    desc = commands[name].desc
+    desc_str = ''
+    if desc:
+        desc_str = ' ' + desc
+    args = commands[name].args
+    args_str = ''
+    if args:
+        args_str = ' ' + args
+    return '`{state.prefix}{name}{args_str}`{desc_str}'.format(**locals())
 
 
-@register_cmd('help', 'help [command]: Show help for command, or for all commands')
+@register_cmd('help', args='[command]', desc='Show help for command, or for all commands')
 async def cmd_help(state, message, cmd_name=None, *args):
     if not cmd_name:
-        response = 'Available commands:'
+        responses = []
         for cmd in commands.values():
             if cmd.hidden:
                 continue
-            response += '\n  ' + fmt_cmd_usage(state, cmd.name)
-        await message.channel.send(response)
+            responses.append(fmt_cmd_usage(state, cmd.name))
+        await message.channel.send('\n'.join(responses))
         return
 
     if cmd_name in commands:
@@ -52,7 +60,7 @@ async def cmd_help(state, message, cmd_name=None, *args):
         await message.channel.send('No such command: {cmd_name}\nTry {state.prefix}help'.format(**locals()))
 
 
-@register_cmd('reload', 'reload: reload commands', hidden=True)
+@register_cmd('reload', desc='reload commands', hidden=True)
 async def cmd_reload(state, message, *args):
     if message.author.id != admin_user_id:
         await message.channel.send('Not authenticated')
@@ -70,32 +78,39 @@ async def cmd_test(state, message, *args):
     pass
 
 
-@register_cmd('say', 'say: ttk2 Reddit quote')
+@register_cmd('react', args='<emoji> [emoji...]', desc='react with all these emoji')
+async def cmd_react(state, message, *emojis):
+    for emoji in emojis:
+        await message.add_reaction(emoji)
+
+
+@register_cmd('say', desc='ttk2 Reddit quote')
 async def cmd_say(state, message, *args):
     await message.channel.send(random.choice(ttk2_strings))
 
 
-@register_cmd('sleep', 'sleep: sleep 5s')
+@register_cmd('sleep', desc='sleep 5s')
 async def cmd_sleep(state, message, *args):
     with message.channel.typing():
         await asyncio.sleep(5.0)
         await message.channel.send('Done sleeping.')
 
 
-@register_cmd('reworld', 'reworld: recreate world from scratch')
+@register_cmd('reworld', desc='recreate world from scratch')
 async def cmd_reworld(state, message, *args):
-    with message.channel.typing():
-        state.world = world.create_new()
-        await message.channel.send('Created new world')
+    await message.add_reaction(emojis.hourglass_flowing_sand)
+    state.world = world.create_new()
+    await message.add_reaction(emojis.checkmark)
+    await message.remove_reaction(emojis.hourglass_flowing_sand, state.client.user)
 
 
-@register_cmd('hi', 'hi: join game')
+@register_cmd('hi', desc='join game')
 async def cmd_hi(state, message, *args):
     player = state.world.get_or_create_player(message.author.id)
     await message.channel.send(message.author.name + ' joined the game world')
 
 
-@register_cmd('inventory', 'inventory: show carried items')
+@register_cmd('inventory', desc='show carried items')
 async def cmd_inventory(state, message, *args):
     player = state.world.get_or_create_player(message.author.id)
     items = player.inventory
@@ -104,17 +119,18 @@ async def cmd_inventory(state, message, *args):
     else:
         msg = message.author.name + ' has these items:'
         for i, item in enumerate(items):
-            msg += '\n{}: {}'.format(i, item)
+            msg += '\n`{}`: {}'.format(i, item)
         await message.channel.send(msg)
 
 
-@register_cmd('craft', 'craft <item> [item...]: combine items')
+@register_cmd('craft', args='<item> [item...]', desc='combine items')
 async def cmd_craft(state, message, *args):
     player = state.world.get_or_create_player(message.author.id)
     await message.channel.send('TODO: implement crafting')
+    await message.add_reaction(emojis.hourglass_flowing_sand)
 
 
-@register_cmd('travel', 'travel <destination>: travel to destination channel')
+@register_cmd('travel', args='<destination>', desc='travel to destination channel')
 async def cmd_travel(state, message, destination=None, *args):
     if not destination:
         await message.channel.send('Usage: ' + fmt_cmd_usage(state, 'travel'))
@@ -127,9 +143,12 @@ async def cmd_travel(state, message, destination=None, *args):
         dest_channel = discord.utils.get(guild.channels, name=destination)
 
         if dest_channel == message.channel:
-            await message.channel.send('You are already in #{}'.format(destination))
-            await message.react(':no_entry_sign:')
+            await message.channel.send('You are already in <#{}>'.format(dest_channel.id))
+            await message.add_reaction(emojis.no_entry_sign)
             return
+
+        player = state.world.get_or_create_player(author.id)
+        # XXX for testing, create region if not exists
 
         # for testing, create channel if not exists
         if not dest_channel:
@@ -142,10 +161,8 @@ async def cmd_travel(state, message, destination=None, *args):
                 destination,
                 overwrites=overwrites, category=category, reason='cmd_travel')
 
-        player = state.world.get_or_create_player(author.id)
-        # XXX for testing, create region if not exists
-        # XXX remove player from current region
         # XXX add player to destination region
+        # XXX remove player from current region
 
         await dest_channel.set_permissions(author, read_messages=True, send_messages=True)
         await message.channel.set_permissions(author, overwrite=None)
@@ -158,8 +175,7 @@ def init_state(client, state):
     if not state:
         state = Autodict()
     if not state.prefix:
-        state.prefix = '.'
-    state.prefix = '.'
+        state.prefix = ','
     if not state.client:
         state.client = client
     if not state.world:
@@ -171,6 +187,8 @@ async def handle_message(state, message):
     if not message.content.startswith(state.prefix):
         return
     cmd_name, *args = message.content[len(state.prefix):].split(' ')
+    if not cmd_name:
+        return
 
     if cmd_name in commands:
         print('Running command', message.author, message.content)
@@ -178,9 +196,13 @@ async def handle_message(state, message):
             await commands[cmd_name].func(state, message, *args)
         except Exception as e:
             print('Handler for', cmd_name, 'failed on', message.content)
+            await message.add_reaction(emojis.no_entry_sign)
             raise e
     else:
-        await message.channel.send('No such command {state.prefix}{cmd_name}, try {state.prefix}help'.format(**locals()))
+        await message.add_reaction(emojis.question_mark)
+        await respond_on_react(
+            state, message, emojis.question_mark,
+            'No such command `{state.prefix}{cmd_name}`, try `{state.prefix}help`'.format(**locals()))
 
 
 async def handle_reaction_change(state, reaction, user):
@@ -189,3 +211,18 @@ async def handle_reaction_change(state, reaction, user):
 
 async def handle_member_join(state, member):
     pass
+
+
+async def respond_on_react(state, message, emoji, response):
+    def check(reaction, user):
+        return user == message.author and str(reaction.emoji) == emoji
+
+    try:
+        reaction, user = await state.client.wait_for(
+            'reaction_add', check=check, timeout=60)
+    except asyncio.TimeoutError:
+        pass
+    else:
+        await message.channel.send(response)
+
+    await message.remove_reaction(emoji, state.client.user)
---
+++ civcord/emojis.py
@@ -0,0 +1,54 @@
+# control
+checkmark = '✅'
+no_entry_sign = '🚫'
+question_mark = '❓'
+stop_sign = '🛑'
+construction_site = '🏗'
+construction = '🚧'
+stopwatch = '⏱'
+hourglass_flowing_sand = '⏳'
+hourglass = '⌛'
+
+hours = '🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚'
+
+# items
+pick = '⛏'
+hammer = '🔨'
+gem = '💎'
+bomb = '💣'
+package = '📦'
+scroll = '📜'
+page = '📄'
+
+# terrain
+mountain_snow = '🏔'
+mount_fuji = '🗻'
+mountain = '⛰'
+desert = '🏜'
+beach = '🏖'
+fire = '🔥'
+
+# plants
+deciduous_tree = '🌳'
+evergreen_tree = '🌲'
+palm_tree = '🌴'
+seedling = '🌱'
+ear_of_rice = '🌾'
+
+# buildings
+camping = '🏕'
+house = '🏠'
+houses = '🏘'
+cityscape = '🏙'
+abandoned = '🏚'
+classical_building = '🏛'
+
+# vehicles
+canoe = '🛶'
+sailboat = '⛵'
+speedboat = '🚤'
+ship = '🚢'
+tractor = '🚜'
+truck = '🚚'
+airplane = '✈'
+rocket = '🚀'
